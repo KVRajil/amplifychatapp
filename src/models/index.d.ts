209372e4f2e0af37d68b249962a51667851bed5c
KVRajil <rajil.kv@byjus.com>
2022-06-17 12:30:08 +0530

    realtime chat app using aws appsync

---
+++ src/models/index.d.ts
@@ -0,0 +1,32 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+type UserMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+type MessageMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+export declare class User {
+  readonly id: string;
+  readonly name?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+  constructor(init: ModelInit<User, UserMetaData>);
+  static copyOf(source: User, mutator: (draft: MutableModel<User, UserMetaData>) => MutableModel<User, UserMetaData> | void): User;
+}
+
+export declare class Message {
+  readonly id: string;
+  readonly user?: string | null;
+  readonly text?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+  constructor(init: ModelInit<Message, MessageMetaData>);
+  static copyOf(source: Message, mutator: (draft: MutableModel<Message, MessageMetaData>) => MutableModel<Message, MessageMetaData> | void): Message;
+}
